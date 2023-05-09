bca0679f9be2ae36585caef75a71e6a5fbe8deb2
Jon Krohn <jonkrohn@users.noreply.github.com>
2023-05-09 08:42:12 -0400

    add Shaan's multi-GPU files

---
+++ code/Finetune-T5-multiGPU.py
@@ -0,0 +1,194 @@
+import pytorch_lightning as pl
+from transformers import T5ForConditionalGeneration
+from transformers import AutoTokenizer
+from torch.utils.data import DataLoader
+from transformers import get_linear_schedule_with_warmup
+from torch.optim import AdamW
+import os
+from peft import get_peft_model, LoraConfig, TaskType
+import generate_data
+import torch
+import argparse
+from pytorch_lightning.loggers import TensorBoardLogger
+import os, json
+from torch.utils.data import Dataset
+from pytorch_lightning.utilities.deepspeed import (
+    convert_zero_checkpoint_to_fp32_state_dict,
+)
+from torch.utils.data import default_collate
+
+
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
+
+
+class T5Finetuner(pl.LightningModule):
+    def __init__(self, args, train_data, val_data):
+        super().__init__()
+        self.save_hyperparameters()
+        self.args = args
+        self.model = T5ForConditionalGeneration.from_pretrained(
+            self.args.model_name,
+            cache_dir=args.cache,
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            args.model_name, cache_dir=args.cache, use_fast=True
+        )
+        self.cache_dir = args.cache
+
+        self.train_data, self.val_data = train_data, val_data
+
+        self.validation_step_outputs = []
+        self.get_peft()
+
+    def get_peft(self):
+        self.model.enable_input_require_grads()
+        peft_config = LoraConfig(
+            task_type=TaskType.SEQ_2_SEQ_LM,
+            inference_mode=False,
+            r=8,
+            lora_alpha=32,
+            lora_dropout=0.1,
+        )
+        self.model = get_peft_model(self.model, peft_config)
+        self.model.print_trainable_parameters()
+
+    def forward(self, batch, batch_idx):
+        return self.model(**batch)
+
+    def training_step(self, batch, batch_idx):
+        # accumulation: https://lightning.ai/docs/fabric/latest/advanced/gradient_accumulation.html
+        loss = self(batch, batch_idx)[0]
+        return {"loss": loss, "log": {"train_loss": loss}}
+
+    def validation_step(self, batch, batch_idx):
+        loss = self(batch, batch_idx)[0]
+        self.validation_step_outputs.append(loss)
+        return {"loss": loss}
+
+    def on_validation_epoch_end(self):
+        torch.stack(self.validation_step_outputs).mean()
+        self.validation_step_outputs.clear()
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_data,
+            batch_size=self.args.batch_size,
+            num_workers=os.cpu_count(),
+            pin_memory=True,
+            collate_fn=default_collate,
+            prefetch_factor=50,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_data,
+            batch_size=self.args.batch_size,
+            num_workers=os.cpu_count(),
+            pin_memory=True,
+            collate_fn=default_collate,
+            prefetch_factor=50,
+        )
+
+    def configure_optimizers(self):
+        optimizer = AdamW(
+            self.trainer.model.parameters(), lr=self.args.lr, weight_decay=0.01
+        )
+        scheduler = get_linear_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=0,
+            num_training_steps=self.args.epochs
+            * len(self.train_data)
+            / self.args.batch_size,
+        )
+        return {"optimizer": optimizer, "lr_scheduler": scheduler}
+
+
+class StreamingDataset(Dataset):
+    def __init__(self, path, model_name):
+        self.path = path
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_name, cache_dir=args.cache, use_fast=True
+        )
+
+    def __len__(self):
+        return len(os.listdir(self.path))
+
+    def encode_text(self, text_input, text_ouput):
+        inputs = self.tokenizer(
+            text_input,
+            max_length=16,
+            truncation=True,
+            padding="max_length",
+            return_tensors="pt",
+        )
+        labels = self.tokenizer(
+            text_ouput,
+            max_length=16,
+            truncation=True,
+            padding="max_length",
+            return_tensors="pt",
+        ).input_ids[0]
+        input_ids = inputs["input_ids"][0]
+        attention_mask = inputs["attention_mask"][0]
+        labels = torch.tensor([label if label != 0 else -100 for label in labels])
+        return {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+            "labels": labels,
+        }
+
+    def __getitem__(self, idx):
+        file_path = os.path.join(self.path, str(idx) + ".json")
+        with open(file_path, "r") as infile:
+            data = json.load(infile)
+        number, words = str(data["number"]), data["words"]
+        return self.encode_text(number, words)
+
+
+def start_training(args):
+    generate_data.main(args.train_size, args.val_size)
+    train_data = StreamingDataset(os.path.join(args.data, "train"), args.model_name)
+    val_data = StreamingDataset(os.path.join(args.data, "val"), args.model_name)
+
+    summarizer = T5Finetuner(args, train_data, val_data)
+    # https://lightning.ai/docs/pytorch/stable/advanced/training_tricks.html?highlight=gradient%20accumulation
+    # checkpointing: https://lightning.ai/docs/pytorch/stable/advanced/model_parallel.html#activation-checkpointing
+    trainer = pl.Trainer(
+        max_epochs=args.epochs,
+        accelerator="gpu",
+        devices="auto",
+        precision="16-mixed",
+        accumulate_grad_batches=4,
+        strategy="deepspeed_stage_3",  # https://lightning.ai/docs/pytorch/latest/extensions/strategy.html#:~:text=The%20Strategy%20in%20PyTorch%20Lightning,%2C%20broadcast%2C%20and%20so%20on.
+        check_val_every_n_epoch=1,
+        logger=TensorBoardLogger(
+            os.path.join(args.output, "logs"), name=args.model_name
+        ),
+        log_every_n_steps=1,
+    )
+    trainer.fit(summarizer)
+
+    ckpt_path = f"./output/logs/{args.model_name}/version_{trainer.logger.version}/checkpoints/epoch={trainer.current_epoch-1}-step={trainer.global_step}.ckpt"
+
+    convert_zero_checkpoint_to_fp32_state_dict(ckpt_path, "lightning_model.pt")
+    training_model = T5Finetuner.load_from_checkpoint("lightning_model.pt")
+    training_model.model.save_pretrained("digit_conversion")
+    
+    # run `huggingface-cli login` or `poetry run huggingface-cli login`
+    # training_model.model.push_to_hub("digit_conversion")
+    # training_model.tokenizer.push_to_hub("digit_conversion")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--data", default="./data/")
+    parser.add_argument("-c", "--cache", default="./cache/")
+    parser.add_argument("-o", "--output", default="./output/")
+    parser.add_argument("-t", "--train_size", default=20_000)
+    parser.add_argument("-v", "--val_size", default=2_000)
+    parser.add_argument("-m", "--model_name", default="t5-base")
+    parser.add_argument("-l", "--lr", default=1e-05)
+    parser.add_argument("-e", "--epochs", default=10)
+    parser.add_argument("-b", "--batch_size", default=16)
+    args = parser.parse_args()
+    start_training(args)
---
+++ code/generate_data.py
@@ -0,0 +1,154 @@
+## large portions of this adapted from https://docs.mosaicml.com/projects/streaming/en/stable/examples/synthetic_nlp.html
+
+from typing import Any, Dict, List, Tuple
+
+import numpy as np
+from tqdm import tqdm
+import json, os
+
+
+ones = (
+    "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen "
+    + "fifteen sixteen seventeen eighteen nineteen"
+).split()
+
+tens = "twenty thirty forty fifty sixty seventy eighty ninety".split()
+
+
+def say(i: int) -> List[str]:
+    """Get the word form of a number.
+
+    Args:
+        i (int): The number.
+
+    Returns:
+        List[str]: The number in word form.
+    """
+    if i < 0:
+        return ["negative"] + say(-i)
+    elif i <= 19:
+        return [ones[i]]
+    elif i < 100:
+        return [tens[i // 10 - 2]] + ([ones[i % 10]] if i % 10 else [])
+    elif i < 1_000:
+        return [ones[i // 100], "hundred"] + (say(i % 100) if i % 100 else [])
+    elif i < 1_000_000:
+        return say(i // 1_000) + ["thousand"] + (say(i % 1_000) if i % 1_000 else [])
+    elif i < 1_000_000_000:
+        return (
+            say(i // 1_000_000)
+            + ["million"]
+            + (say(i % 1_000_000) if i % 1_000_000 else [])
+        )
+    else:
+        assert False
+
+
+def get_random_number() -> int:
+    """Pick a random number the way humans would.
+
+    Picked numbers are positively skewed, exponentially distributed (good for curriculum learning).
+
+    Returns:
+        int: The number.
+    """
+    sign = (np.random.random() < 0.8) * 2 - 1
+    mag = 10 ** np.random.uniform(1, 4) - 10
+    return sign * int(mag**2)
+
+
+def get_numbers(num_train: int, num_val: int) -> Tuple[List[int], List[int]]:
+    """Get two non-overlapping splits of unique random numbers.
+
+    Because the distribution is exponential, we are unlikely to run out of numbers.
+
+    Args:
+        num_train (int): Number of training samples.
+        num_val (int): Number of validation samples.
+
+    Returns:
+        Tuple[List[int], List[int]]: The two generated splits.
+    """
+    total = num_train + num_val
+    numbers = set()
+    bar = tqdm(total=total, leave=False)
+    while len(numbers) < total:
+        was = len(numbers)
+        numbers.add(get_random_number())
+        bar.update(len(numbers) - was)
+    numbers = list(numbers)
+    np.random.shuffle(numbers)
+    return numbers[:num_train], numbers[num_train:]
+
+
+def generate_samples(numbers: List[int]) -> List[Dict[str, Any]]:
+    """Generate samples from a list of numbers.
+
+    Args:
+        numbers (List[int]): The numbers.
+
+    Returns:
+        List[Dict[str, Any]]: The corresponding samples.
+    """
+    samples = []
+    for num in numbers:
+        words = " ".join(say(num))
+        sample = {"number": num, "words": words}
+        samples.append(sample)
+    return samples
+
+
+def get_dataset(
+    num_train: int, num_val: int
+) -> Tuple[List[Dict[str, Any]], List[Dict[str, Any]]]:
+    """Generate a number-saying dataset of the given size.
+
+    Args:
+        num_train (int): Number of training samples.
+        num_val (int): Number of validation samples.
+
+    Returns:
+        Tuple[List[Dict[str, Any]], List[Dict[str, Any]]]: The two generated splits.
+    """
+    train_nums, val_nums = get_numbers(num_train, num_val)
+    train_samples = generate_samples(train_nums)
+    val_samples = generate_samples(val_nums)
+    return train_samples, val_samples
+
+
+def create_folder_structure():
+    if not os.path.isdir("./data/"):
+        os.mkdir("./data/")
+    if not os.path.isdir("./data/train/"):
+        os.mkdir("./data/train/")
+    if not os.path.isdir("./data/val/"):
+        os.mkdir("./data/val/")
+
+    for f in os.listdir("./data/train/"):
+        os.remove(os.path.join("./data/train", f))
+    for f in os.listdir("./data/val/"):
+        os.remove(os.path.join("./data/val", f))
+
+
+def main(num_train: int, num_val: int):
+    print(f"Generating synthetic dataset ({num_train} train, {num_val} val)...")
+    train_samples, val_samples = get_dataset(num_train, num_val)
+
+    create_folder_structure()
+
+    for i in range(len(train_samples)):
+        with open(f"./data/train/{i}.json", "w") as outfile:
+            json.dump(train_samples[i], outfile)
+
+    for j in range(len(val_samples)):
+        with open(f"./data/val/{j}.json", "w") as outfile:
+            json.dump(val_samples[j], outfile)
+
+
+if __name__ == "__main__":
+    # Number of training and validation samples
+    num_train_samples = 10_000  # 10k samples
+    num_val_samples = 2000  # 2k samples
+
+    # Create the samples.
+    main(num_train_samples, num_val_samples)
